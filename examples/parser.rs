e55d967f0e6f8957210e99fdf639e3c4faf30a63
baishen <baishen2009@gmail.com>
2024-01-17 00:04:24 +0800

    add example

---
+++ examples/parser.rs
@@ -0,0 +1,37 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use std::fs::File;
+use std::io::{BufReader, Seek};
+use std::time::Instant;
+
+use jsonb::new_parser::Parser;
+
+// cargo run --example parser --release
+fn main() -> Result<()> {
+
+     let s = r#"{"abcd":12.34,                                                                                   "xxaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxxaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxxaaaaaaaaaaaaaaaaaaa    aaaaaaaaaaaaaaaaaaxxxaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxxaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxxaaaaaaa    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxxaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxxaaaaa    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxaaaaaaaaaxxxaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxxxaaaaaaaaaaaaaaaaaaaaaaa    aaaaaaaaaaaaaax":444.55, "glossary":{"title":"example glossary","GlossDiv":{"title":"S","GlossList":            {"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":        "SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages     such as DocBook.","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}}"#.to_string();
+
+    println!("s={:?}", s);
+    let data = s.into_bytes();
+
+    let mut parser = Parser::new();
+    let buf = parser.parse(&data).unwrap();
+    println!("buf={:?}", buf);
+
+    Ok(())
+}
